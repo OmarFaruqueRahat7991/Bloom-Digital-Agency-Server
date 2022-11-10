803c2c67723d53647daddc319fa071a84ac86ae7
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-11-10 20:50:55 +0600

    another commit

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db("bloomDigitalAgency").collection("services");
+        // const orderCollection = client.db("carDoctor").collection("orders");
+
+        app.get("/services", async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+          });
+        app.get("/service", async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+          });
+    }
+    finally{
+
+    }
+}
+run().catch((error) => console.error(error));
+
 
 app.get('/', (req, res) => {
   res.send('My Eleventh Assignment Server Is Running');
