a0ec1df8a8028de7d276f1abe31fa7d149d135cb
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-11-10 19:28:04 +0600

    another commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 // const jwt = require('jsonwebtoken');
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
   res.send('My Eleventh Assignment Server Is Running');
