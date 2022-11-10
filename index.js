2175b1157837713ca532e69c609caf67cedaf5fd
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-11-10 21:26:05 +0600

    another commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 // const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,12 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
+          });
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const oneService = await serviceCollection.findOne(query);
+            res.send(oneService);
           });
     }
     finally{
